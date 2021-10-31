ce187ea6dadb4374455d6ddcc4ad7288c775099f
Sanjaana26 <93467314+Sanjaana26@users.noreply.github.com>
2021-10-31 16:27:18 +0530

    Add files via upload

---
+++ Automatic whatsapp message.py.py
@@ -0,0 +1,9 @@
+import pywhatkit as kit
+import time
+import pyautogui as pg
+import keyboard as k
+kit.sendwhatmsg("+919003250520","HAPPY BIRTHDAY ",8,26)
+pg.click(1050,950)
+time.sleep(2)
+k.press_and_release("enter")
+
